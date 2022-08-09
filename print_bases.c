7bb658101a5ae77ec7d199111a65da9a893cfdcb
Marvellouschris <marvellousekhatorik@gmail.com>
2022-08-09 02:31:24 -0700

    updated

--- print_bases.c
+++ print_bases.c
@@ -9,6 +9,7 @@
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
  * Return: the number of char printed
+ * updating printf_base.c
  */
 int print_hex(va_list l, flags_t *f)
 {
